b9cee7db8f486c6a63a789ac70fe420a65b924fc
Vignesh Esakkiappan <beingvig@gmail.com>
2026-06-05 10:12:06 +0530

    bluetooth icon

--- waybar/scripts/bluetooth.sh
+++ waybar/scripts/bluetooth.sh
@@ -5,5 +5,5 @@ device=$(bluetoothctl devices Connected | cut -d' ' -f3-)
 if [ -n "$device" ]; then
     echo "<span font_size='10pt'>󰂱</span> $device"
 else
-    echo "<span font_size='10pt'>󰂱</span>"
+    echo "<span font_size='10pt'></span>"
 fi
